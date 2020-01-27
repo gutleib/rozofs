307c52823ebb9865b3026f534068777629dae07b
rozodopamine <jean-pierre.monchanin@rozofs.com>
2020-01-27 09:18:13 +0100

    storage label set : do not call system   Instead of calling e2label via a system() call to set the labal on a disk   the storage now directly reads and then rewrites the superblock of the disk   updating its labal. This enables to avoid a system() call that the mellanox   driver does not seem to like while RDMA traffic is on going.

--- src/storaged/storage_enumeration.c
+++ src/storaged/storage_enumeration.c
@@ -36,6 +36,7 @@
 #include <sys/mount.h>
 #include <sys/resource.h>
 #include <mntent.h>
+#include <linux/magic.h>
 
 #include <rozofs/rozofs.h>
 #include <rozofs/common/log.h>
@@ -60,6 +61,9 @@ storage_enumerated_device_t * storage_enumerated_device_tbl[STORAGE_MAX_DEV_PER_
 int                           storage_enumerated_device_nb=0;
 
 int oldlsblk = 1;
+
+#define LABELSIZE           16
+#define OFFSET_SUPERBLOCK 1024
 /*
  *_______________________________________________________________________
  *
@@ -67,27 +71,141 @@ int oldlsblk = 1;
  *
  * @param pDev       Enumerated device context address
  */
- void rozofs_set_label(storage_enumerated_device_t * pDev) {
-   char label[ROZOFS_LABEL_SIZE];
-   char cmd[256];
-   
-   if (pDev->spare) {
-     char * pt = label;
-     pt += sprintf(label, "%s", ROZOFS_REGULAR_LABEL); 
-     if (pDev->spare_mark) {
-       pt += sprintf(label, "%s",pDev->spare_mark);  
-     } 
-   }
-   else {
-     sprintf(label, ROZOFS_REGULAR_LABEL ,pDev->cid, pDev->sid, pDev->dev);  
-   }
-     
-   if (strcmp(pDev->label,label) == 0) return;
-   
-   sprintf(cmd, "e2label %s %s", pDev->pName->name, label);
-   if (system(cmd)==0) {
-     strcpy(pDev->label, label);
-   }
+void rozofs_set_label(storage_enumerated_device_t * pDev) {
+  char label[ROZOFS_LABEL_SIZE];
+  int  fd = -1;
+  /*
+  ** Ext2 superblock
+  */
+  struct ext2_super_block {
+    uint32_t s_inodes_count;		/* Inodes count */
+    uint32_t s_blocks_count;		/* Blocks count */
+    uint32_t s_r_blocks_count;	/* Reserved blocks count */
+    uint32_t s_free_blocks_count;	/* Free blocks count */
+    uint32_t s_free_inodes_count;	/* Free inodes count */
+    uint32_t s_first_data_block;	/* First Data Block */
+    uint32_t s_log_block_size;	/* Block size */
+    int32_t  s_log_frag_size;	/* Fragment size */
+    uint32_t s_blocks_per_group;	/* # Blocks per group */
+    uint32_t s_frags_per_group;	/* # Fragments per group */
+    uint32_t s_inodes_per_group;	/* # Inodes per group */
+    uint32_t s_mtime;		/* Mount time */
+    uint32_t s_wtime;		/* Write time */
+    uint16_t s_mnt_count;		/* Mount count */
+    int16_t  s_max_mnt_count;	/* Maximal mount count */
+    uint8_t  s_magic[2];		/* Magic signature */
+    uint16_t s_state;		/* File system state */
+    uint16_t s_errors;		/* Behaviour when detecting errors */
+    uint16_t s_minor_rev_level; 	/* minor revision level */
+    uint32_t s_lastcheck;		/* time of last check */
+    uint32_t s_checkinterval;	/* max. time between checks */
+    uint32_t s_creator_os;		/* OS */
+    uint32_t s_rev_level;		/* Revision level */
+    uint16_t s_def_resuid;		/* Default uid for reserved blocks */
+    uint16_t s_def_resgid;		/* Default gid for reserved blocks */
+    /*
+    * These fields are for EXT2_DYNAMIC_REV superblocks only.
+    *
+    * Note: the difference between the compatible feature set and
+    * the incompatible feature set is that if there is a bit set
+    * in the incompatible feature set that the kernel doesn't
+    * know about, it should refuse to mount the filesystem.
+    * 
+    * e2fsck's requirements are more strict; if it doesn't know
+    * about a feature in either the compatible or incompatible
+    * feature set, it must abort and not try to meddle with
+    * things it doesn't understand...
+    */
+    uint32_t s_first_ino; 		/* First non-reserved inode */
+    uint16_t s_inode_size; 		/* size of inode structure */
+    uint16_t s_block_group_nr; 	/* block group # of this superblock */
+    uint32_t s_feature_compat; 	/* compatible feature set */
+    uint32_t s_feature_incompat; 	/* incompatible feature set */
+    uint32_t s_feature_ro_compat; 	/* readonly-compatible feature set */
+    unsigned char s_uuid[16];		/* 128-bit uuid for volume */
+    char     s_volume_name[LABELSIZE]; 	/* volume name */
+    char     s_last_mounted[64]; 	/* directory where last mounted */
+    uint32_t s_algorithm_usage_bitmap; /* For compression */
+    /*
+    * Performance hints.  Directory preallocation should only
+    * happen if the EXT2_COMPAT_PREALLOC flag is on.
+    */
+    unsigned char s_prealloc_blocks;	/* Nr of blocks to try to preallocate*/
+    unsigned char s_prealloc_dir_blocks;	/* Nr to preallocate for dirs */
+    uint16_t s_padding1;
+    uint32_t s_reserved[204];	/* Padding to the end of the block */
+  } sb;
+  
+  if (pDev->spare) {
+    char * pt = label;
+    pt += sprintf(label, "%s", ROZOFS_SPARE_LABEL); 
+    if (pDev->spare_mark) {
+      pt += sprintf(label, "%s",pDev->spare_mark);  
+    } 
+  }
+  else {
+    sprintf(label, ROZOFS_REGULAR_LABEL ,pDev->cid, pDev->sid, pDev->dev);  
+  }
+
+  if (strcmp(pDev->label,label) == 0) return;
+
+  /*
+  ** Open device
+  */
+  fd = open(pDev->pName->name, O_RDWR);
+  if (fd < 0) {
+    severe("open(%s) %s", pDev->pName->name, strerror(errno));
+    return;
+  }
+  /*
+  ** Seek superblock at offset 1024
+  */
+  if (lseek(fd, OFFSET_SUPERBLOCK, SEEK_SET) != OFFSET_SUPERBLOCK) {
+    severe("lseek(%s) %s", pDev->pName->name, strerror(errno));
+    close(fd);
+    return;
+  }
+  /*
+  ** Read superblock
+  */
+  if (read(fd, (char *) &sb, sizeof(sb)) != sizeof(sb)) {
+    severe("read(%s) %s", pDev->pName->name, strerror(errno));
+    goto out;
+  }
+  /*
+  ** Check superblock magic number
+  */
+   if (sb.s_magic[0] + 256*sb.s_magic[1] != EXT2_SUPER_MAGIC) {
+    severe("%s no ext2 superblock 0x%x%x", pDev->pName->name,sb.s_magic[1],sb.s_magic[0]);
+    goto out;
+  }
+
+  /*
+  ** Re-write label
+  */
+  memset(sb.s_volume_name, 0, LABELSIZE);
+  strncpy(sb.s_volume_name, label, LABELSIZE);
+  if (strlen(label) > LABELSIZE) {
+    severe("%s Label \"%s\" too long \"%s\"", pDev->pName->name,label, sb.s_volume_name);
+  }  
+  /*
+  ** Seek superblock at offset 1024
+  */
+  if (lseek(fd, OFFSET_SUPERBLOCK, SEEK_SET) != OFFSET_SUPERBLOCK) {
+    severe("lseek(%s) %s", pDev->pName->name, strerror(errno));
+    goto out;
+  }
+  /*
+  ** Re-wite superblock
+  */
+  if (write(fd, (char *) &sb, sizeof(sb)) != sizeof(sb)) {
+    severe("lseek(%s) %s", pDev->pName->name, strerror(errno));
+    goto out;
+  }
+  info("Label \"%s\" set on %s", sb.s_volume_name, pDev->pName->name);
+  
+out:
+  if (fd>0) close(fd); 
 }   
 /*
  *_______________________________________________________________________
